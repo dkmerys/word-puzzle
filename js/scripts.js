af82a00fc216b358bca2bcf3c4cf55e4abe86ec0
Daniel Merys <dkmerys@gmail.com>
2020-03-31 14:14:26 -0700

    rework loops, but not make them actually work

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
 
   $("#wordpuzzle").submit(function(event) {
+    event.preventDefault();
     var sentence = $("#sentence").val();
+    var sentenceArray = sentence.split("");
     
     var vowels = ["a", "e", "i", "o", "u"];
-    var index = 0
+    
 
-    for (index = 0; index < 5; index ++) {
-      sentence = sentence.replace(vowels[index], "-");
-      alert(sentence)
-    }
-
-    event.preventDefault();
+    for (var i = 0; i < sentenceArray.length; i++) {
+      for (var j=0; j < vowels.length; i++) {
+        if(sentenceArray[i] === vowels[j])
+          sentenceArray[i] = "-";
+      };
+    };
+    sentence = sentenceArray.join();
+    alert(sentence);
   });
 });
+// var input = "123,44 44,4 56";
+// var inputArr = input.split("");
+// console.log(inputArr);
+
+// var nums = [1,2,3,4,5,6,7,8];
+// var outputArray = [];
+// for(var i = 0; i < inputArr.length; i++){
+//   for(var j=0; j < nums.length; i++){
+//     if(inputArr[i] === nums[j])
+//
