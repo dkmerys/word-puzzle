a8ae84972a6629c194815827a5471b5f578455b0
Daniel Merys <dkmerys@gmail.com>
2020-03-31 15:12:19 -0700

    update js to functioning

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,23 @@ $(document).ready(function() {
 
   $("#wordpuzzle").submit(function(event) {
     event.preventDefault();
+    $("#sentence").hide();
     var sentence = $("#sentence").val();
     var sentenceArray = sentence.split("");
-    
+    console.log(sentenceArray)
+
     var vowels = ["a", "e", "i", "o", "u"];
     
 
     for (var i = 0; i < sentenceArray.length; i++) {
-      for (var j=0; j < vowels.length; i++) {
-        if(sentenceArray[i] === vowels[j])
+      for (var j=0; j < vowels.length; j++) {
+        if(sentenceArray[i] === vowels[j]) {
           sentenceArray[i] = "-";
+        };  
       };
     };
-    sentence = sentenceArray.join();
-    alert(sentence);
+    sentence = sentenceArray.join('');
+    $("#puzzle").append(sentenceArray).show();
   });
 });
 // var input = "123,44 44,4 56";
