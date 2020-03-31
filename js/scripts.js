a6a24ed621b056c6fc5874de560555dd4b222490
Daniel Merys <dkmerys@gmail.com>
2020-03-31 11:27:17 -0700

    add non-functional js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+  $("button").click(function(event) {
+    var sentece = $("#sentence").val();
+
+    var vowels = ["a", "e", "i", "o", "u"];
+    for (var index = 0; sentence.includes(vowels); sentence.replace ([vowels], "-")) {
+      return 
+    }
+    $("#puzzle").show()
+    event.preventDefault();
+  });
 });
